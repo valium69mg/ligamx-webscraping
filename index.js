b46e3af54a00b0f80fe41bf3dc18994f422597bb
valium69mg <xdeus.v2@gmail.com>
2024-08-26 16:20:19 -0600

    Separate the search data (web scraping) from the index.js code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import pg from 'pg'
 const { Client } = pg
-import {By, Builder, Browser} from 'selenium-webdriver';
+import searchData from './searchData.js';
 
 // database config
 const client = new Client({
@@ -49,99 +49,6 @@ async function saveData(data,tableName) {
     }
 };
 
-// web scrapping config 
-let nameSubfix = '/th';
-let posSubfix = '/td[2]';
-let countrySubfix = '/td[1]/a/span/span';
-let numberOfMatchesSubfix = '/td[4]';
-let numberOfStartingMatchesSubfix = '/td[5]';
-let numberOfMinutesPlayedSubfix = '/td[6]';
-let numberOfGoalsSubfix = '/td[8]';
-let numberOfAssistSubfix = '/td[9]';
-let numberOfYellowCardsSubfix = '/td[14]';
-let numberOfRedCardsSubfix = '/td[15]';
-let numberOfProgressiveCarryingSubfix = '/td[20]';
-let numberOfProgressivePassesSubfix = '/td[21]';
-let numberOfProgressivePassesReceivedSubfix = '/td[22]';
-
-async function searchData(url,noOfPlayers,xpathPrefix) {
-  let driver;
-  try {
-    // build driver
-    driver = await new Builder().forBrowser(Browser.CHROME).build();
-    // setup driver connection
-    await driver.get(url);
-    let data = [];
-    // loop through table to retrieve data into a list of dictionaries
-    for (let i = 1; i <= noOfPlayers; i++) {
-        let playerStats = {};
-        let row = "tr[" + i + "]" ;
-        // id 
-        let id = i;
-        playerStats['id'] = id;
-        // name 
-        let namePath = xpathPrefix + row + nameSubfix;
-        let name = await driver.findElement(By.xpath(namePath)).getText();
-        playerStats['name'] = name;
-        // post
-        let posPath = xpathPrefix + row + posSubfix;
-        let pos = await driver.findElement(By.xpath(posPath)).getText();
-        playerStats['pos'] = pos;
-        // country 
-        let countryPath = xpathPrefix + row + countrySubfix;
-        let country = await driver.findElement(By.xpath(countryPath)).getText();
-        playerStats['country'] = country;
-        // no of matches
-        let numberOfMatchesPath = xpathPrefix + row + numberOfMatchesSubfix;
-        let numberOfMatches = await driver.findElement(By.xpath(numberOfMatchesPath )).getText();
-        playerStats['numberOfMatches'] = numberOfMatches;
-        // no of starting matches
-        let numberOfStartingMatchesPath = xpathPrefix + row + numberOfStartingMatchesSubfix;
-        let numberOfStartingMatches = await driver.findElement(By.xpath(numberOfStartingMatchesPath)).getText();
-        playerStats['numberOfStartingMatches'] = numberOfStartingMatches;
-        // no of minutes played 
-        let numberOfMinutesPlayedPath = xpathPrefix + row + numberOfMinutesPlayedSubfix;
-        let numberOfMinutesPlayed = await driver.findElement(By.xpath(numberOfMinutesPlayedPath)).getText();
-        playerStats['numberOfMinutesPlayed'] = numberOfMinutesPlayed;
-        // no of goals
-        let numberOfGoalsPath = xpathPrefix + row + numberOfGoalsSubfix;
-        let numberOfGoals = await driver.findElement(By.xpath(numberOfGoalsPath)).getText();
-        playerStats['numberOfGoals'] = numberOfGoals;
-        // no of assist
-        let numberOfAssistPath = xpathPrefix + row + numberOfAssistSubfix;
-        let numberOfAssist = await driver.findElement(By.xpath(numberOfAssistPath)).getText();
-        playerStats['numberOfAssist'] = numberOfAssist;
-        // no of yellow cards
-        let numberOfYellowCardsPath = xpathPrefix + row + numberOfYellowCardsSubfix;
-        let numberOfYellowCards = await driver.findElement(By.xpath(numberOfYellowCardsPath)).getText();
-        playerStats['numberOfYellowCards'] = numberOfYellowCards;
-        // no of red cards
-        let numberOfRedCardsPath = xpathPrefix + row + numberOfRedCardsSubfix;
-        let numberOfRedCards = await driver.findElement(By.xpath(numberOfRedCardsPath)).getText();
-        playerStats['numberOfRedCards'] = numberOfRedCards;
-        // progressive carrying
-        let numberOfProgressiveCarryingPath = xpathPrefix + row + numberOfProgressiveCarryingSubfix;
-        let numberOfProgressiveCarrying = await driver.findElement(By.xpath(numberOfProgressiveCarryingPath)).getText();
-        playerStats['numberOfProgressiveCarrying'] = numberOfProgressiveCarrying;
-        // progressive passes
-        let numberOfProgressivePassesPath = xpathPrefix + row + numberOfProgressivePassesSubfix;
-        let numberOfProgressivePasses = await driver.findElement(By.xpath(numberOfProgressivePassesPath)).getText();
-        playerStats['numberOfProgressivePasses'] = numberOfProgressivePasses;
-        // progressive passes received        
-        let numberOfProgressivePassesReceivedPath = xpathPrefix + row + numberOfProgressivePassesReceivedSubfix;
-        let numberOfProgressivePassesReceived = await driver.findElement(By.xpath(numberOfProgressivePassesReceivedPath)).getText();
-        playerStats['numberOfProgressivePassesReceived'] = numberOfProgressivePassesReceived;
-        // retrieve data
-        data.push(playerStats);
-    }
-    // if succesfull return data
-    return data;
-  } catch (e) {
-    console.log(e);
-  } finally {
-    await driver.quit();
-  }
-};
 // cruz azul
 
 let xpathPrefix = '//*[@id="stats_standard_31"]/tbody/';
@@ -166,6 +73,6 @@ const barcelonaUrl2223 = "https://fbref.com/es/equipos/206d90db/2022-2023/c12/Es
 // 21-22 season stats link
 const barcelonaUrl2122 = "https://fbref.com/en/squads/206d90db/2021-2022/s11323/Barcelona-Stats";
 
-let data = await searchData(barcelonaUrl2122,barcelonaPlayersCount,barcelonaXpathPrefix2122 );
+let data = await searchData(cruzAzulUrl,cruzAzulPlayersCount,xpathPrefix);
 
-await saveData(data, 'fc_barcelona_stats_21_22');
+await saveData(data, 'cruz_azul_stats_24_25');
---
+++ searchData.js
@@ -0,0 +1,95 @@
+import {By, Builder, Browser} from 'selenium-webdriver';
+
+// web scrapping config 
+let nameSubfix = '/th';
+let posSubfix = '/td[2]';
+let countrySubfix = '/td[1]/a/span/span';
+let numberOfMatchesSubfix = '/td[4]';
+let numberOfStartingMatchesSubfix = '/td[5]';
+let numberOfMinutesPlayedSubfix = '/td[6]';
+let numberOfGoalsSubfix = '/td[8]';
+let numberOfAssistSubfix = '/td[9]';
+let numberOfYellowCardsSubfix = '/td[14]';
+let numberOfRedCardsSubfix = '/td[15]';
+let numberOfProgressiveCarryingSubfix = '/td[20]';
+let numberOfProgressivePassesSubfix = '/td[21]';
+let numberOfProgressivePassesReceivedSubfix = '/td[22]';
+
+export default async function searchData(url,noOfPlayers,xpathPrefix) {
+  let driver;
+  try {
+    // build driver
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    // setup driver connection
+    await driver.get(url);
+    let data = [];
+    // loop through table to retrieve data into a list of dictionaries
+    for (let i = 1; i <= noOfPlayers; i++) {
+        let playerStats = {};
+        let row = "tr[" + i + "]" ;
+        // id 
+        let id = i;
+        playerStats['id'] = id;
+        // name 
+        let namePath = xpathPrefix + row + nameSubfix;
+        let name = await driver.findElement(By.xpath(namePath)).getText();
+        playerStats['name'] = name;
+        // post
+        let posPath = xpathPrefix + row + posSubfix;
+        let pos = await driver.findElement(By.xpath(posPath)).getText();
+        playerStats['pos'] = pos;
+        // country 
+        let countryPath = xpathPrefix + row + countrySubfix;
+        let country = await driver.findElement(By.xpath(countryPath)).getText();
+        playerStats['country'] = country;
+        // no of matches
+        let numberOfMatchesPath = xpathPrefix + row + numberOfMatchesSubfix;
+        let numberOfMatches = await driver.findElement(By.xpath(numberOfMatchesPath )).getText();
+        playerStats['numberOfMatches'] = numberOfMatches;
+        // no of starting matches
+        let numberOfStartingMatchesPath = xpathPrefix + row + numberOfStartingMatchesSubfix;
+        let numberOfStartingMatches = await driver.findElement(By.xpath(numberOfStartingMatchesPath)).getText();
+        playerStats['numberOfStartingMatches'] = numberOfStartingMatches;
+        // no of minutes played 
+        let numberOfMinutesPlayedPath = xpathPrefix + row + numberOfMinutesPlayedSubfix;
+        let numberOfMinutesPlayed = await driver.findElement(By.xpath(numberOfMinutesPlayedPath)).getText();
+        playerStats['numberOfMinutesPlayed'] = numberOfMinutesPlayed;
+        // no of goals
+        let numberOfGoalsPath = xpathPrefix + row + numberOfGoalsSubfix;
+        let numberOfGoals = await driver.findElement(By.xpath(numberOfGoalsPath)).getText();
+        playerStats['numberOfGoals'] = numberOfGoals;
+        // no of assist
+        let numberOfAssistPath = xpathPrefix + row + numberOfAssistSubfix;
+        let numberOfAssist = await driver.findElement(By.xpath(numberOfAssistPath)).getText();
+        playerStats['numberOfAssist'] = numberOfAssist;
+        // no of yellow cards
+        let numberOfYellowCardsPath = xpathPrefix + row + numberOfYellowCardsSubfix;
+        let numberOfYellowCards = await driver.findElement(By.xpath(numberOfYellowCardsPath)).getText();
+        playerStats['numberOfYellowCards'] = numberOfYellowCards;
+        // no of red cards
+        let numberOfRedCardsPath = xpathPrefix + row + numberOfRedCardsSubfix;
+        let numberOfRedCards = await driver.findElement(By.xpath(numberOfRedCardsPath)).getText();
+        playerStats['numberOfRedCards'] = numberOfRedCards;
+        // progressive carrying
+        let numberOfProgressiveCarryingPath = xpathPrefix + row + numberOfProgressiveCarryingSubfix;
+        let numberOfProgressiveCarrying = await driver.findElement(By.xpath(numberOfProgressiveCarryingPath)).getText();
+        playerStats['numberOfProgressiveCarrying'] = numberOfProgressiveCarrying;
+        // progressive passes
+        let numberOfProgressivePassesPath = xpathPrefix + row + numberOfProgressivePassesSubfix;
+        let numberOfProgressivePasses = await driver.findElement(By.xpath(numberOfProgressivePassesPath)).getText();
+        playerStats['numberOfProgressivePasses'] = numberOfProgressivePasses;
+        // progressive passes received        
+        let numberOfProgressivePassesReceivedPath = xpathPrefix + row + numberOfProgressivePassesReceivedSubfix;
+        let numberOfProgressivePassesReceived = await driver.findElement(By.xpath(numberOfProgressivePassesReceivedPath)).getText();
+        playerStats['numberOfProgressivePassesReceived'] = numberOfProgressivePassesReceived;
+        // retrieve data
+        data.push(playerStats);
+    }
+    // if succesfull return data
+    return data;
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await driver.quit();
+  }
+};
