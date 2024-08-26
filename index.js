2a6ece1503148693f20e7b20d1eb4e662439905a
valium69mg <xdeus.v2@gmail.com>
2024-08-26 16:16:19 -0600

    Add one more season to the fc barcelona database

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ async function searchData(url,noOfPlayers,xpathPrefix) {
   }
 };
 // cruz azul
+
 let xpathPrefix = '//*[@id="stats_standard_31"]/tbody/';
 // rows on website (26 players)
 const cruzAzulPlayersCount = 26;
@@ -153,13 +154,18 @@ const cruzAzulUrl = "https://fbref.com/es/equipos/632f1838/Estadisticas-de-Cruz-
 
 // barcelona
 let barcelonaXpathPrefix = '//*[@id="stats_standard_12"]/tbody/';
+let barcelonaXpathPrefix2122 = '//*[@id="stats_standard_8"]/tbody/';
 // rows on website (23 players)
 const barcelonaPlayersCount = 23;
 // 24-25 season stats link
 const barcelonaUrl = "https://fbref.com/es/equipos/206d90db/Estadisticas-de-Barcelona";
 // 23-24 season stats link
 const barcelonaUrl2324 = "https://fbref.com/es/equipos/206d90db/2023-2024/c12/Estadisticas-de-Barcelona-La-Liga";
+// 22-23 season stats link
+const barcelonaUrl2223 = "https://fbref.com/es/equipos/206d90db/2022-2023/c12/Estadisticas-de-Barcelona-La-Liga";
+// 21-22 season stats link
+const barcelonaUrl2122 = "https://fbref.com/en/squads/206d90db/2021-2022/s11323/Barcelona-Stats";
 
-let data = await searchData(cruzAzulUrl,cruzAzulPlayersCount,xpathPrefix);
+let data = await searchData(barcelonaUrl2122,barcelonaPlayersCount,barcelonaXpathPrefix2122 );
 
-await saveData(data, 'cruz_azul_stats_24_25');
+await saveData(data, 'fc_barcelona_stats_21_22');
