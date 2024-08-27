86cee5a95e513861931d8de3bd7da31a0638813b
valium69mg <xdeus.v2@gmail.com>
2024-08-27 08:26:43 -0600

    Separate index from analyzeSeason and add conditionals to the search and save data section

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import searchData from './searchData.js';
 import { saveData , retrieveData } from './postgresDriver.js';
-
+import analyzeSeason from './analyzeSeason.js';
 //========================== LA LIGA =========================================//
 
 // FC BARCELONA
@@ -55,6 +55,9 @@ const atlasUrl = "https://fbref.com/es/equipos/7c76bc53/Estadisticas-de-Atlas";
 const atlasXpath = '//*[@id="stats_standard_31"]/tbody/';
 const atlasPlayerCount = 20;
 // PACHUCA
+const pachucaUrl = "https://fbref.com/es/equipos/1be8d2e3/Estadisticas-de-Pachuca";
+const ligamxXpath = '//*[@id="stats_standard_31"]/tbody/';
+const ligamxPlayerCount = 20;
 // SANTOS LAGUNA
 // CLUB TIJUANA
 // LEON
@@ -64,109 +67,15 @@ const atlasPlayerCount = 20;
 // JUAREZ
 // QUERETARO
 // ATL SAN LUIS
+
 //=============================== SEARCH AND SAVE DATA =======================//
 
 // PROGRAM TO SEARCH AND SAVE DATA
-//let searchedData = await searchData(atlasUrl,atlasPlayerCount,atlasXpath);
-//await saveData(searchedData, 'atlas_stats_24_25');
-
-//=============================== ANALYZE DATA ====================================//
-
-// ANALYZE DATA 
-// ANALYZE 1 SEASON OF A TEAM
-// MOST GOALS, MOST ASSISTS, MOST G/A, MOST Progression, Most Time Played, Most Cards
-// tableName = fc_barcelona_stats_24_25
-
-async function analyzeSeason(tableName) {
-
-  let results = await retrieveData(tableName);
-  // loop through results
-  // goals, assists and g/a
-  let maxGoals = 0;
-  let maxGoalsName;
-  let maxAssists = 0;
-  let maxAssistsName;
-  let maxGoalsAndAssists = 0;
-  let maxGoalsAndAssistsName;
-  // progressions
-  let maxProgressionCarries = 0;
-  let maxProgressionsCarriesName;
-  let maxProgressionPass = 0;
-  let maxProgressionPassName;
-  let maxProgressionReceived = 0;
-  let maxProgressionReceivedName;
-  // time played
-  let maxTimePlayed = 0;
-  let maxTimePlayedName;
-  // cards
-  let maxYellowCards = 0;
-  let maxYellowCardsName;
-  let maxRedCards = 0;
-  let maxRedCardsName;
-  for (let i = 0; i < results.length; i++) {
-      let player = results[i];
-      let goalsAndAssist = 0;
-      let cards = 0;
-      // GOALS
-      if (player['number_of_goals'] !== '' && parseInt(player['number_of_goals']) > maxGoals) {
-        maxGoals = parseInt(player['number_of_goals']); 
-        maxGoalsName = player['name'];
-        goalsAndAssist += parseInt(player['number_of_goals']);
-      }
-      // ASSISTS
-      if (player['number_of_assist'] !== '' && parseInt(player['number_of_assist']) > maxAssists) {
-        maxAssists = parseInt(player['number_of_assist']); 
-        maxAssistsName = player['name'];
-        goalsAndAssist += parseInt(player['number_of_assist']);
-      }
-      // G/A
-      if (goalsAndAssist > maxGoalsAndAssists) {
-        maxGoalsAndAssists = goalsAndAssist;
-        maxGoalsAndAssistsName = player['name'];
-      }
-      // MINUTES PLAYED
-      if (player['number_of_minutes_played'] !== '' && parseInt(player['number_of_minutes_played']) > maxTimePlayed) {
-        maxTimePlayed = parseInt(player['number_of_minutes_played']);
-        maxTimePlayedName = player['name'];
-      }
-      // PROGRESSIONS
-      if (player['number_of_progressive_carrying'] !== '' && parseInt(player['number_of_progressive_carrying']) > maxProgressionCarries) {
-        maxProgressionCarries = parseInt(player['number_of_progressive_carrying']);
-        maxProgressionsCarriesName = player['name'];
-      }
-      if (player['number_of_progressive_pass'] !== '' && parseInt(player['number_of_progressive_pass']) > maxProgressionPass) {
-        maxProgressionPass = parseInt(player['number_of_progressive_pass']);
-        maxProgressionPassName = player['name'];
-      }
-      if (player['number_of_progressive_pass_received'] !== '' && parseInt(player['number_of_progressive_pass_received']) > maxProgressionReceived) {
-        maxProgressionReceived = parseInt(player['number_of_progressive_pass_received']);
-        maxProgressionReceivedName = player['name'];
-      }
-      // CARDS
-      if (player['number_of_yellow_cards'] !== '' && parseInt(player['number_of_yellow_cards']) > maxYellowCards) {
-        maxYellowCards = parseInt(player['number_of_yellow_cards']);
-        maxYellowCardsName = player['name'];
-      }
-      if (player['number_of_red_cards'] !== '' && parseInt(player['number_of_red_cards']) > maxRedCards) {
-        maxRedCards = parseInt(player['number_of_red_cards']);
-        maxRedCardsName = player['name']; 
-      }
-  }
-  
-  let analyzedData = {
-    maxGoals: {amount: maxGoals, name: maxGoalsName},
-    maxAssists: {amount: maxAssists,name: maxAssistsName},
-    maxGoalsAndAssists: {amount: maxGoalsAndAssists,name: maxGoalsAndAssistsName},
-    maxProgressionCarries: {amount: maxProgressionCarries, name: maxProgressionsCarriesName},
-    maxProgressionPass: {amount: maxProgressionPass, name: maxProgressionPassName},
-    maxProgressionReceived: {amount: maxProgressionReceived, name: maxProgressionReceivedName},
-    maxTimePlayed: {amount: maxTimePlayed, name: maxTimePlayedName},
-    maxYellowCards: {amount: maxYellowCards, name: maxYellowCardsName},
-    maxRedCards: {amount: maxRedCards, name: maxRedCardsName},
-  };
-  
-  return analyzedData;
-};
+let tableName = 'pachuca_stats_24_25';
+let searchedData = await searchData(pachucaUrl,ligamxXpath,ligamxPlayerCount);
+if (searchData != []) { // IF SEARCH WAS SUCCESFUL
+  await saveData(searchedData, tableName); // returns true or false
+}
 
 //let data = await analyzeSeason('atlas_stats_24_25');
 //console.log(data);
