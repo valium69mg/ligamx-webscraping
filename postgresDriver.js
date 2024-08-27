8aa4d7581de59ad0f78133e4c9d5c8788112acc6
valium69mg <xdeus.v2@gmail.com>
2024-08-27 09:51:37 -0600

    Change handle error on checkTableDate

--- postgresDriver.js
+++ postgresDriver.js
@@ -46,7 +46,6 @@ async function checkTableDate(tableName) {
         let updated_at = results.rows[0].updated_at;
         return updated_at;
     }catch(e) {
-        console.log(new Error(`relation ${tableName} does not exist`));
         return null;
     }finally {
         await client.end();
