cdac25465e79ba3fdb34bc7d381845f5e9e66341
valium69mg <xdeus.v2@gmail.com>
2024-08-26 18:59:32 -0600

    Add more teams from liga mx

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 import searchData from './searchData.js';
 import { saveData , retrieveData } from './postgresDriver.js';
 
-// CRUZ AZUL
-let xpathPrefix = '//*[@id="stats_standard_31"]/tbody/';
-// rows on website (26 players)
-const cruzAzulPlayersCount = 26;
-// 23-24 season stats link
-const cruzAzulUrl2324 = "https://fbref.com/es/equipos/632f1838/2023-2024/Estadisticas-de-Cruz-Azul";
-// 24-25 season stats link
-const cruzAzulUrl = "https://fbref.com/es/equipos/632f1838/Estadisticas-de-Cruz-Azul";
+//========================== LA LIGA =========================================//
 
 // FC BARCELONA
 let barcelonaXpathPrefix = '//*[@id="stats_standard_12"]/tbody/';
@@ -23,24 +16,65 @@ const barcelonaUrl2324 = "https://fbref.com/es/equipos/206d90db/2023-2024/c12/Es
 const barcelonaUrl2223 = "https://fbref.com/es/equipos/206d90db/2022-2023/c12/Estadisticas-de-Barcelona-La-Liga";
 // 21-22 season stats link
 const barcelonaUrl2122 = "https://fbref.com/en/squads/206d90db/2021-2022/s11323/Barcelona-Stats";
+//=============================== LIGA MX ====================================//
 
+// CRUZ AZUL
+let xpathPrefix = '//*[@id="stats_standard_31"]/tbody/';
+// rows on website (26 players)
+const cruzAzulPlayersCount = 26;
+// 23-24 season stats link
+const cruzAzulUrl2324 = "https://fbref.com/es/equipos/632f1838/2023-2024/Estadisticas-de-Cruz-Azul";
+// 24-25 season stats link
+const cruzAzulUrl = "https://fbref.com/es/equipos/632f1838/Estadisticas-de-Cruz-Azul";
 // CHIVAS
 const chivasUrl = "https://fbref.com/es/equipos/c02b0f7a/Estadisticas-de-Guadalajara";
 const chivasXpathPrefix = '//*[@id="stats_standard_31"]/tbody/';
 const chivasPlayerCount = 25;
-
 // AMERICA
 const americaUrl = "https://fbref.com/es/equipos/18d3c3a3/Estadisticas-de-America";
 const americaXpath = '//*[@id="stats_standard_31"]/tbody/';
 const americaPlayerCount = 28;
+// PUMAS 
+const pumasUrl = "https://fbref.com/es/equipos/c9d59c6c/Estadisticas-de-Pumas-UNAM";
+const pumasXpath = '//*[@id="stats_standard_31"]/tbody/';
+const pumasPlayerCount = 25;
+// TOLUCA
+const tolucaUrl = "https://fbref.com/es/equipos/44b88a4e/Estadisticas-de-Toluca";
+const tolucaXpath  = '//*[@id="stats_standard_31"]/tbody/';
+const tolucaPlayerCount = 25;
+// TIGRES
+const tigresUrl = "https://fbref.com/es/equipos/d9e1bd51/Estadisticas-de-UANL";
+const tigresXpath = '//*[@id="stats_standard_31"]/tbody/';
+const tigresPlayerCount = 23;
+// MONTERREY
+const monterreyUrl = "https://fbref.com/es/equipos/dd5ca9bd/Estadisticas-de-Monterrey";
+const monterreyXpath ='//*[@id="stats_standard_31"]/tbody/';
+const monterreyPlayerCount = 21;
+// ATLAS
+const atlasUrl = "https://fbref.com/es/equipos/7c76bc53/Estadisticas-de-Atlas";
+const atlasXpath = '//*[@id="stats_standard_31"]/tbody/';
+const atlasPlayerCount = 20;
+// PACHUCA
+// SANTOS LAGUNA
+// CLUB TIJUANA
+// LEON
+// NECAXA
+// PUEBLA
+// MAZATLAN
+// JUAREZ
+// QUERETARO
+// ATL SAN LUIS
+//=============================== SEARCH AND SAVE DATA =======================//
+
 // PROGRAM TO SEARCH AND SAVE DATA
-//let data = await searchData(americaUrl,americaPlayerCount,americaXpath);
-// await saveData(data, 'america_stats_24_25');
+//let searchedData = await searchData(atlasUrl,atlasPlayerCount,atlasXpath);
+//await saveData(searchedData, 'atlas_stats_24_25');
+
+//=============================== ANALYZE DATA ====================================//
 
 // ANALYZE DATA 
 // ANALYZE 1 SEASON OF A TEAM
 // MOST GOALS, MOST ASSISTS, MOST G/A, MOST Progression, Most Time Played, Most Cards
-
 // tableName = fc_barcelona_stats_24_25
 
 async function analyzeSeason(tableName) {
@@ -134,5 +168,5 @@ async function analyzeSeason(tableName) {
   return analyzedData;
 };
 
-let data = await analyzeSeason('america_stats_24_25');
-console.log(data);
+//let data = await analyzeSeason('atlas_stats_24_25');
+//console.log(data);
