60ad7a11d7562f16b45849171c6b89b31f7973f6
valium69mg <xdeus.v2@gmail.com>
2024-08-27 18:41:00 -0600

    Add commentary to searchAndSave.js function

--- searchAndSaveData.js
+++ searchAndSaveData.js
@@ -1,10 +1,27 @@
 import searchData from './searchData.js';
 import { checkTableDate ,checkIfUpdateNeeded,saveData  } from './postgresDriver.js';
+import {printTableNameLog} from './consolePrint.js';
+
+// function: searchAndSaveData
+
+// description: this function takes four parameters
+
+// teamUrl => this is the URL from the FBREF website provided in the lists of index.js of each
+// team
+// teamXpath => each league has an specific xpath that uses, it is also on index.js variables
+// for each team
+// teamPlayerCount => amount of players records that we want to save
+// tableName => the table's name on the database, also in the list of index.js for each team
+
+// the goal of this function is to search and update the data on the database if needed.
+// the function first checks if the table exists, if there is no table with that name
+// it begins the process to create it by searching the web and then saving the data, if the
+// table is found on the registry, it checks if it needs updating, if it does it updates it,
+// if the table is up to date then if just returns with nothing.
 
 export default async function searchAndSaveData(teamUrl,teamXpath,teamPlayerCount,tableName) {
     // PROGRAM TO SEARCH AND SAVE DATA
-    console.log("\n");
-    console.log(`============== ${tableName} ==========`)
+    printTableNameLog(tableName);
     let updated_at = await checkTableDate(tableName);
     if (updated_at === null) { // THERE IS NOT A TABLE WITH THAT NAME
       console.log("Table not found, searching info...");
